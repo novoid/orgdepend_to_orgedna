97f3f765fee337957d73d1410a7b304936151e02
Karl Voit <git@Karl-Voit.at>
2020-09-19 23:41:31 +0200

    unit test script, README, removed old script

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 # pytest is invoking the unit tests:
-PYTHONPATH=. pytest-3 org-depend_to_org-edna_test.py
+PYTHONPATH=. pytest-3 orgdepend_to_orgedna_test.py
 
 # mypy is checking the type annotations:
-#PYTHONPATH=. mypy orgformat/orgformat.py
+PYTHONPATH=. mypy orgdepend_to_orgedna.py
 
 #end
