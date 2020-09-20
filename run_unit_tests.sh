110235f411f1c8f915dee10aafc60b8feed2bfa4
Karl Voit <git@Karl-Voit.at>
2020-09-20 22:04:30 +0200

    run_unit_tests.sh: better stdout formatting

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+FILENAME=$(basename $0)
 set -o errexit
 
-# pytest is invoking the unit tests:
+report()
+{
+    echo "\n–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––→"
+    echo "$FILENAME:  $@"
+    echo "←–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––\n"
+}
+
+report "pytest is invoking the unit tests:"
 PYTHONPATH=. pytest-3 orgdepend_to_orgedna_test.py
 
-# mypy is checking the type annotations:
+report "mypy is checking the type annotations:"
 PYTHONPATH=. mypy orgdepend_to_orgedna.py
 
+report "all tests finished without an issue :-)"
 #end
