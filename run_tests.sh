575d94128eeb6590cac94fc9f24ade95fb90091a
Karl Voit <Karl.Voit@RISE-World.com>
2020-09-24 19:41:14 +0200

    added end-to-end test with demo Org file conversion

--- run_tests.sh
+++ run_tests.sh
@@ -15,5 +15,11 @@ pytest
 report "mypy is checking the type annotations:"
 PYTHONPATH=. mypy orgdepend_to_orgedna.py
 
+report "converting \"testfile_input.org\" to \"testfile_input_converted.org\""
+./orgdepend_to_orgedna.py --overwrite testfile_input.org
+
+report "comparing \"testfile_input_converted.org\" to \"testfile_expected_output.org\""
+diff testfile_input_converted.org testfile_expected_output.org
+
 report "all tests finished without an issue :-)"
 #end
